6beed8e3905a0882478592a14a8f133bc6412141
jbkenemer <89149632+jbkenemer@users.noreply.github.com>
2021-11-12 12:34:41 -0500

    Added saved file bc error in git commit

--- SpaceTravel.py
+++ SpaceTravel.py
@@ -1,12 +1,14 @@
+#imports for this project
 import urllib.request as urllib2
-
 import json
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import geoplotlib
 
 req = urllib2.Request("http://api.open-notify.org/iss-now.json")
 response = urllib2.urlopen(req)
 
-obj = json.loads(response.read())
+obj = json.loads(response.read()
 
-print(obj)
-print(obj['timestamp'])
-print(obj['iss_position']['latitude'], obj['data']['iss_position']['latitude'])
+
